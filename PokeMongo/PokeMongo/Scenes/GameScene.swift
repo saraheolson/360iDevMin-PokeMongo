003b716fd31f9ddf9663945e771602deb1cc3971
Sarah Olson <solson@trello.com>
2016-10-28 08:02:41 -0400

    MOVING BALL: Ball moves along with user's touch gestures.

--- PokeMongo/PokeMongo/Scenes/GameScene.swift
+++ PokeMongo/PokeMongo/Scenes/GameScene.swift
@@ -17,6 +17,10 @@ class GameScene: SKScene {
     // So we can keep track of our ball!
     var ball: SKSpriteNode?
 
+    // Defines where user touched
+    var startTouchLocation: CGPoint = CGPoint.zero
+    var endTouchLocation: CGPoint = CGPoint.zero
+
     override func sceneDidLoad() {
         
         // If we don't already have a monster, create one.
@@ -29,28 +33,6 @@ class GameScene: SKScene {
             createBall()
         }
     }
-    
-    // MARK: - Ball functions
-    
-    /**
-     * Create a ball node and add it to the scene.
-     */
-    func createBall() {
-        
-        // Create the ball node
-        ball = SKSpriteNode(imageNamed: "Ball")
-        if let ball = ball {
-            
-            // Set position and scale
-            ball.position = CGPoint(x: 0, y: 100)
-            ball.zPosition = 2
-            ball.scale(to: CGSize(width: 50, height: 50))
-            
-            // Add to the scene
-            addChild(ball)
-        }
-    }
-
     
     // MARK: - Monster functions
     
@@ -112,24 +94,58 @@ class GameScene: SKScene {
      *  Called when a touch event is initiated.
      */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Record the beginning of the touch event
+        startTouchLocation = touches.first!.location(in: self)
     }
     
     /**
      *  Called when a touch event moves.
      */
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Move the ball along with the user's touch gesture
+        let currentTouchLocation = touches.first!.location(in: self)
+        if let ball = ball {
+            ball.position = currentTouchLocation
+        }
     }
     
     /**
      *  Called when a touch event is ended.
      */
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Record the end of the touch event
+        endTouchLocation = touches.first!.location(in: self)
     }
     
     /**
      *  Called before each frame is rendered.
      */
     override func update(_ currentTime: TimeInterval) {
+        
     }
     
+    
+    // MARK: - Ball functions
+    
+    /**
+     * Create a ball node and add it to the scene.
+     */
+    func createBall() {
+        
+        // Create the ball node
+        ball = SKSpriteNode(imageNamed: "Ball")
+        if let ball = ball {
+            
+            // Set position and scale
+            ball.position = CGPoint(x: 0, y: 100)
+            ball.zPosition = 2
+            ball.scale(to: CGSize(width: 50, height: 50))
+            
+            // Add to the scene
+            addChild(ball)
+        }
+    }
 }
